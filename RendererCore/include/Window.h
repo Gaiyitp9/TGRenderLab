b119cb5ca0ffcd95e04136ce29d3d930a5178a68
Gaiyitp9 <932404058@qq.com>
2021-06-24 00:59:31 +0800

    增加Window类

--- RendererCore/include/Window.h
+++ RendererCore/include/Window.h
@@ -1,16 +1,44 @@
+/********************************************************
+* Copyright (c) LiaoChenhan. All rights reserved.		*
+*														*
+* This code is licensed under the MIT License (MIT).	*
+* THIS CODE IS PROVIDED *AS IS* WITHOUT WARRANTY OF		*
+* ANY KIND, EITHER EXPRESS OR IMPLIED, INCLUDING ANY	*
+* IMPLIED WARRANTIES OF FITNESS FOR A PARTICULAR		*
+* PURPOSE, MERCHANTABILITY, OR NON-INFRINGEMENT.		*
+*********************************************************/
+
 #pragma once
 
-#include <Windows.h>
+#include "pch.h"
 
 namespace LCH
 {
+	enum class WindowType
+	{
+		Default
+	};
+
+	class WindowClassRegister
+	{
+	public:
+		WindowClassRegister();
+	public:
+		std::unordered_map<WindowType, std::string> windowClassMap;
+	};
+
 	class Window
 	{
 	public:
-		Window();
+		Window(HINSTANCE hInstance, float width, float height, WindowType type = WindowType::Default);
 		~Window();
+
+	private:
+		// ´°¿Ú×¢²áÆ÷
+		static WindowClassRegister wcRegister;
 
 	public:
 		HWND hwnd;
+		HINSTANCE hInstance;
 	};
 }
