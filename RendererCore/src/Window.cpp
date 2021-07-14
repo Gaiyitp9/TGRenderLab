da4a1e9faf4f076cdcca47e8e1034288ee7ee37b
Gaiyitp9 <932404058@qq.com>
2021-07-14 23:39:17 +0800

    更新Window类

--- RendererCore/src/Window.cpp
+++ RendererCore/src/Window.cpp
@@ -22,7 +22,29 @@ namespace IGGSZLab
 		WNDCLASSEX wc = {};
 		wc.cbSize = sizeof(WNDCLASSEX);
 		wc.style = CS_HREDRAW | CS_VREDRAW;
-		wc.lpfnWndProc = WindowProc;
+		wc.lpfnWndProc = WindowProcSetup;
+		wc.cbClsExtra = 0;
+		wc.cbWndExtra = 0;
+		wc.hInstance = GetModuleHandle(nullptr);
+	}
+
+	LRESULT WindowClassRegister::WindowProcSetup(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
+	{
+		if (msg == WM_NCCREATE)
+		{
+			const CREATESTRUCT* const pCreate = reinterpret_cast<CREATESTRUCT*>(lParam);
+			Window* const pWnd = static_cast<Window*>(pCreate->lpCreateParams);
+			SetWindowLongPtr(hwnd, GWLP_USERDATA, reinterpret_cast<LONG_PTR>(pWnd));
+			SetWindowLongPtr(hwnd, GWLP_WNDPROC, reinterpret_cast<LONG_PTR>(WindowProcThunk));
+			return pWnd->WindowProc(hwnd, msg, wParam, lParam);
+		}
+		return DefWindowProc(hwnd, msg, wParam, lParam);
+	}
+
+	LRESULT WindowClassRegister::WindowProcThunk(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
+	{
+		Window* const pWnd = reinterpret_cast<Window*>(GetWindowLongPtr(hwnd, GWLP_USERDATA));
+		return pWnd->WindowProc(hwnd, msg, wParam, lParam);
 	}
 
 	const WindowClassRegister* WindowClassRegister::GetInstance()
@@ -33,13 +55,13 @@ namespace IGGSZLab
 		return instance;
 	}
 
-	LRESULT CALLBACK WindowProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
+	Window::Window(HINSTANCE hInstance, float width, float height, WindowType type)
 	{
 
 	}
 
-	Window::Window(HINSTANCE hInstance, float width, float height, WindowType type)
+	LRESULT Window::WindowProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
 	{
-
+		return DefWindowProc(hwnd, msg, wParam, lParam);
 	}
 }
