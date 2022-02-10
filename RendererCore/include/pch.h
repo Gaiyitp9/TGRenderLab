94f1b282bfca23244565224a93149797704ad83e
Gaiyitp9 <932404058@qq.com>
2022-02-10 17:52:29 +0800

    增加窗口消息监控

--- RendererCore/include/pch.h
+++ RendererCore/include/pch.h
@@ -24,20 +24,6 @@
 #include <wrl.h>
 #include <tchar.h>
 
-#include <d3d12.h>
-#include <d3dcompiler.h>
-#include <dxgi1_6.h>
-#include "d3dx12.h"
-
-#pragma comment(lib, "d3d12.lib")
-#pragma comment(lib, "d3dcompiler.lib")
-#pragma comment(lib, "dxgi.lib")
-
-#ifdef _DEBUG
-#include <dxgidebug.h>
-#include <iostream>
-#endif
-
 #include <cstdint>
 #include <cstdio>
 #include <string>
