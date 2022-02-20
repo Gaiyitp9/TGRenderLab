1fa2a81b4eb90a57dd8f7885bf35224643cef74b
Gaiyitp9 <932404058@qq.com>
2022-02-20 21:02:30 +0800

    增加WinAPIException类

--- RendererCore/include/Diagnostics/BaseException.h
+++ RendererCore/include/Diagnostics/BaseException.h
@@ -32,8 +32,8 @@ namespace LCH
 		const std::wstring& GetExceptionInfo() const noexcept;
 
 	protected:
-		std::wstring wWhatBuffer;						// 异常信息(宽字符，Unicode)
-		std::string whatBuffer;							// 异常信息(窄字符，GB2312)
+		std::wstring wWhatBuffer;						// 异常信息(宽字符)
+		std::string whatBuffer;							// 异常信息(窄字符)
 		std::vector<StackFrame> stackFrameInfo;			// 栈帧信息
 
 	private:
@@ -42,6 +42,7 @@ namespace LCH
 		HANDLE hProcess;								// 当前进程句柄
 		constexpr static USHORT FRAMESTOCAPTURE = 100;	// 最大追踪栈帧数
 		constexpr static UINT MAXNAMELEN = 255;			// 函数名称最大长度
-		const wchar_t* SEPARATOR = L"\n-----------------------------------------------\n";	// 分隔符
+		const wchar_t* SEPARATOR = L"\n-------------"	// 分隔符
+			"----------------------------------\n";	
 	};
 }
