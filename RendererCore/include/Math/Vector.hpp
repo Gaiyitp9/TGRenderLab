b78b8cd2d7bf8d20db4841e1e41037b3c811c4ab
Gaiyitp9 <932404058@qq.com>
2022-05-06 23:16:07 +0800

    增加vector的add函数

--- RendererCore/include/Math/Vector.hpp
+++ RendererCore/include/Math/Vector.hpp
@@ -12,28 +12,53 @@ namespace LCH::Math
 	template<typename T, size_t Size> requires mathlib_type_and_size<T, Size>
 	class Vector
 	{
-		using simd = simd_trait<T, SimdInstruction<Size>::type>;
+		using simd = simd_trait<T, SimdInstruction<T, Size>::type>;
+		static const size_t loop = Size / simd::DataCount;
+		static const size_t remainder = Size % simd::DataCount;
 	public:
-		Vector()
+		Vector(T x = {})
 		{
-
+			data.fill(x);
 		}
 
-		T operator[](size_t index)
+		const T& operator[](size_t index) const
 		{
 			return data.at(index);
 		}
 
-		Vector operator+(const Vector& v)
+		T& operator[](size_t index)
 		{
+			return data.at(index);
+		}
+
+		Vector operator+(const Vector& vec)
+		{
+			Vector res;
 			if constexpr (support_simd_t)
 			{
-
+				T const* v1 = data.data();
+				T const* v2 = vec.data.data();
+				T* vr = res.data.data();
+				for (size_t i = 0; i < loop; ++i)
+				{
+					simd::add(v1, v2, vr);
+					v1 += simd::DataCount;
+					v2 += simd::DataCount;
+					vr += simd::DataCount;
+				}
+				for (size_t i = 0; i < remainder; ++i)
+				{
+					vr[i] = v1[i] + v2[i];
+				}
 			}
 			else
 			{
-
+				for (size_t i = 0; i < Size; ++i)
+				{
+					res.data[i] = data[i] + vec.data[i];
+				}
 			}
+			return res;
 		}
 
 	private:
@@ -43,7 +68,7 @@ namespace LCH::Math
 	template<typename T> requires mathlib_type_and_size<T, 4>
 	class Vector<T, 4>
 	{
-		using simd = simd_trait<T, SimdInstruction<4>::type>;
+		using simd = simd_trait<T, SimdInstruction<T, 4>::type>;
 
 	public:
 		Vector(T x = {})
