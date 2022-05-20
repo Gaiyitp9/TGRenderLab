cb889e363a8f2fbf84b2e49e32da2dfc79eea752
Gaiyitp9 <932404058@qq.com>
2022-05-20 10:57:26 +0800

    更新Graphic

--- RendererCore/src/Graphics/FrameBuffer.cpp
+++ RendererCore/src/Graphics/FrameBuffer.cpp
@@ -7,11 +7,14 @@
 
 namespace LCH::Graphics
 {
-	FrameBuffer::FrameBuffer(const Window& window)
+	FrameBuffer::FrameBuffer(IDXGIFactory* factory, ID3D11Device* device, Window const* window)
 	{
+		ThrowIfFailed(device->CheckMultisampleQualityLevels(backBufferFormat, sampleCount,
+			&numQualityLevels));
+
 		DXGI_SWAP_CHAIN_DESC swapChainDesc = {};
-		swapChainDesc.BufferDesc.Width = window.Width();
-		swapChainDesc.BufferDesc.Height = window.Height();
+		swapChainDesc.BufferDesc.Width = window->Width();
+		swapChainDesc.BufferDesc.Height = window->Height();
 		swapChainDesc.BufferDesc.Format = backBufferFormat;
 		swapChainDesc.BufferDesc.RefreshRate = refreshRate;
 		swapChainDesc.BufferDesc.Scaling = DXGI_MODE_SCALING_UNSPECIFIED;
@@ -20,19 +23,19 @@ namespace LCH::Graphics
 		swapChainDesc.SampleDesc.Quality = numQualityLevels - 1;
 		swapChainDesc.BufferUsage = DXGI_USAGE_RENDER_TARGET_OUTPUT;
 		swapChainDesc.BufferCount = 1;
-		swapChainDesc.OutputWindow = window.Hwnd();
+		swapChainDesc.OutputWindow = window->Hwnd();
 		swapChainDesc.Windowed = true;
 		swapChainDesc.SwapEffect = DXGI_SWAP_EFFECT_DISCARD;
 
-		/*ThrowIfFailed(pFactory->CreateSwapChain(pDevice.Get(), &swapChainDesc, &pSwapChain));
+		ThrowIfFailed(factory->CreateSwapChain(device, &swapChainDesc, &swapChain));
 
 		ComPtr<ID3D11Resource> pBackBuffer;
-		pSwapChain->GetBuffer(0, IID_PPV_ARGS(&pBackBuffer));
-		pDevice->CreateRenderTargetView(
+		swapChain->GetBuffer(0, IID_PPV_ARGS(&pBackBuffer));
+		device->CreateRenderTargetView(
 			pBackBuffer.Get(),
 			nullptr,
-			&pRenderTargetView
-		);*/
+			&renderTargetView
+		);
 	}
 
 	FrameBuffer::~FrameBuffer()
