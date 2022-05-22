812d1aaa21ed4877332090101423a69860a4aa6c
Gaiyitp9 <932404058@qq.com>
2022-05-22 12:05:08 +0800

    更新D3D11Details

--- RendererCore/include/Graphics/D3D11Details.hpp
+++ RendererCore/include/Graphics/D3D11Details.hpp
@@ -16,84 +16,25 @@ using Microsoft::WRL::ComPtr;
 
 namespace LCH::Graphics
 {
+	DXGI_FORMAT DirectXFormatMapping(Format format);
+
 	template<>
 	class Device<LowLevelAPI::DirectX11>
 	{
 	public:
-		Device()
-		{
-			ThrowIfFailed(CreateDXGIFactory1(IID_PPV_ARGS(&factory)));
-			RetrieveHardwareInfo();
-		}
-
-		ID3D11Device* operator->()
-		{
-			return device.Get();
-		}
-
-		DXGI_FORMAT DirectXFormatMapping(Format format)
-		{
-			static std::unordered_map<Format, DXGI_FORMAT> map =
-			{
-				{Format::R8G8B8A8_UNORM, DXGI_FORMAT_R8G8B8A8_UNORM},
-			};
-			return map[format];
-		}
-
-		std::vector<DXGI_MODE_DESC1> GetOutputModes(Format format, UINT adapterIndex = 0, UINT outputIndex = 0)
-		{
-			DXGI_FORMAT backBufferFormat = DirectXFormatMapping(format);
-
-			ComPtr<IDXGIOutput1> pOutput1;
-			adapterOutputs[adapterIndex][outputIndex].As(&pOutput1);
-			DXGI_OUTPUT_DESC outputDesc;
-			pOutput1->GetDesc(&outputDesc);
-			Debug::LogLine(std::format(L"Device name: {}", outputDesc.DeviceName));
-
-			UINT modeCount = 0;
-			pOutput1->GetDisplayModeList1(backBufferFormat, DXGI_ENUM_MODES_SCALING,
-				&modeCount, nullptr);
-			std::vector<DXGI_MODE_DESC1> outputModes(modeCount);
-			if (modeCount > 0)
-			{
-				pOutput1->GetDisplayModeList1(backBufferFormat, DXGI_ENUM_MODES_SCALING,
-					&modeCount, outputModes.data());
-				for (UINT i = 0; i < modeCount; ++i)
-				{
-					Debug::LogLine(std::format(L"Width: {}  Height: {} Format: {} Refresh rate denominator: {} numerator: {}",
-						outputModes[i].Width, outputModes[i].Height,
-						(UINT)(outputModes[i].Format),
-						outputModes[i].RefreshRate.Denominator,
-						outputModes[i].RefreshRate.Numerator));
-				}
-			}
-			return outputModes;
-		}
+		Device();
+		ID3D11Device* operator->();
+		void GetDeviceContext(Context<LowLevelAPI::DirectX11>* pContext);
+		std::vector<DXGI_MODE_DESC1> GetOutputModes(Format format);
 
 	private:
-		void RetrieveHardwareInfo()
-		{
-			// 枚举所有显示适配器及其输出
-			ComPtr<IDXGIAdapter> pAdapter;
-			for (UINT i = 0; factory->EnumAdapters(i, &pAdapter) != DXGI_ERROR_NOT_FOUND; ++i)
-			{
-				adapters.push_back(pAdapter);
-				DXGI_ADAPTER_DESC desc;
-				pAdapter->GetDesc(&desc);
-				Debug::LogLine(std::format(L"Adpater vendor: {}. Video memory size: {} MB",
-					desc.Description, desc.DedicatedVideoMemory / 1048576));
+		void RetrieveHardwareInfo();
+		void CreateDeviceAndContext();
 
-				std::vector<ComPtr<IDXGIOutput>> outputs;
-				ComPtr<IDXGIOutput> pOutput;
-				for (UINT j = 0; pAdapter->EnumOutputs(j, &pOutput) != DXGI_ERROR_NOT_FOUND; ++j)
-					outputs.push_back(pOutput);
-				adapterOutputs.push_back(outputs);
-			}
-		}
-
-	public:
-		ComPtr<ID3D11Device> device;
+	private:
 		ComPtr<IDXGIFactory> factory;
+		ComPtr<ID3D11Device> device;
+		ComPtr<ID3D11DeviceContext> context;
 		std::vector<ComPtr<IDXGIAdapter>> adapters;
 		std::vector<std::vector<ComPtr<IDXGIOutput>>> adapterOutputs;
 	};
@@ -109,6 +50,7 @@ namespace LCH::Graphics
 
 	private:
 		ComPtr<ID3D11DeviceContext> context;
+		friend void Device<LowLevelAPI::DirectX11>::GetDeviceContext(Context<LowLevelAPI::DirectX11>* pContext);
 	};
 
 	template<>
