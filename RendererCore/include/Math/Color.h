32f339960affb822a33ea73bf219982b602167fe
Gaiyitp9 <932404058@qq.com>
2022-05-23 23:06:20 +0800

    增加Color类

--- RendererCore/include/Math/Color.h
+++ RendererCore/include/Math/Color.h
@@ -6,6 +6,7 @@
 #pragma once
 
 #include "Simd.hpp"
+#include <DirectXColors.h>
 
 namespace LCH::Math
 {
@@ -13,14 +14,54 @@ namespace LCH::Math
 	{
 		using simd = simd_trait<float, SimdInstruction<float, 4>::type>;
 	public:
-		Color();
+		static Color AliceBlue;				// 爱丽丝蓝
+		static Color AntiqueWhite;			// 古董白
+		static Color Aqua;					// 浅绿色
+		static Color Aquamarine;			// 碧绿色
+		static Color Azure;					// 蔚蓝色
+		static Color Beige;					// 米黄色
+		static Color Bisque;				// 橘黄色
+		static Color Black;					// 黑色
+		static Color Blanchedalmond;		// 杏仁白
+		static Color Blue;					// 蓝色
+		static Color BlueViolet;			// 紫罗兰色
+		static Color Brown;					// 棕色
+		static Color BurlyWood;				// 实木色
+		static Color Chartreuse;			// 查特酒绿
+		static Color Chocolate;				// 巧克力色
+		static Color Coral;					// 橘红色
+		static Color CornflowerBlue;		// 矢车菊蓝
+		static Color Cornsilk;				// 玉米丝色
+		static Color Crimson;				// 深红色
+		static Color Cyan;					// 青色
+
+	public:
+		Color() { elements.fill(0); }
+		Color(float r, float g, float b, float a)
+		{
+			elements[0] = r;
+			elements[1] = g;
+			elements[2] = b;
+			elements[3] = a;
+		}
 
 		const float& r() const { return elements[0]; }
-		float g() { return elements[1]; }
-		float b() { return elements[2]; }
-		float a() { return elements[3]; }
+		const float& g() const { return elements[1]; }
+		const float& b() const { return elements[2]; }
+		const float& a() const { return elements[3]; }
+		float& r() { return elements[0]; }
+		float& g() { return elements[1]; }
+		float& b() { return elements[2]; }
+		float& a() { return elements[3]; }
 
+		const float* RGBA() const { return elements.data(); }
 
+		Color operator*(const Color& color)
+		{
+			Color result;
+			simd::elementwise_product(elements.data(), color.elements.data(), result.elements.data());
+			return result;
+		}
 
 	private:
 		aligned_array<float, 4, simd::Alignment> elements;
---
+++ RendererCore/src/Math/Color.cpp
@@ -0,0 +1,31 @@
+/****************************************************************
+* TianGong RenderLab											*
+* Copyright (c) Gaiyitp9. All rights reserved.					*
+* This code is licensed under the MIT License (MIT).			*
+*****************************************************************/
+
+#include "Math/Color.h"
+
+namespace LCH::Math
+{
+	Color Color::AliceBlue		{ 0.941176534f, 0.972549081f, 1.000000000f, 1.000000000f };
+	Color Color::AntiqueWhite	{ 0.980392218f, 0.921568692f, 0.843137324f, 1.000000000f };
+	Color Color::Aqua			{ 0.000000000f, 1.000000000f, 1.000000000f, 1.000000000f };
+	Color Color::Aquamarine		{ 0.498039246f, 1.000000000f, 0.831372619f, 1.000000000f };
+	Color Color::Azure			{ 0.941176534f, 1.000000000f, 1.000000000f, 1.000000000f };
+	Color Color::Beige			{ 0.960784376f, 0.960784376f, 0.862745166f, 1.000000000f };
+	Color Color::Bisque			{ 1.000000000f, 0.894117713f, 0.768627524f, 1.000000000f };
+	Color Color::Black			{ 0.000000000f, 0.000000000f, 0.000000000f, 1.000000000f };
+	Color Color::Blanchedalmond	{ 1.000000000f, 0.921568692f, 0.803921640f, 1.000000000f };
+	Color Color::Blue			{ 0.000000000f, 0.000000000f, 1.000000000f, 1.000000000f };
+	Color Color::BlueViolet		{ 0.541176498f, 0.168627456f, 0.886274576f, 1.000000000f };
+	Color Color::Brown			{ 0.647058845f, 0.164705887f, 0.164705887f, 1.000000000f };
+	Color Color::BurlyWood		{ 0.870588303f, 0.721568644f, 0.529411793f, 1.000000000f };
+	Color Color::Chartreuse		{ 0.498039246f, 1.000000000f, 0.000000000f, 1.000000000f };
+	Color Color::Chocolate		{ 0.823529482f, 0.411764741f, 0.117647067f, 1.000000000f };
+	Color Color::Coral			{ 1.000000000f, 0.498039246f, 0.313725501f, 1.000000000f };
+	Color Color::CornflowerBlue	{ 0.392156899f, 0.584313750f, 0.929411829f, 1.000000000f };
+	Color Color::Cornsilk		{ 1.000000000f, 0.972549081f, 0.862745166f, 1.000000000f };
+	Color Color::Crimson		{ 0.862745166f, 0.078431375f, 0.235294133f, 1.000000000f };
+	Color Color::Cyan			{ 0.000000000f, 1.000000000f, 1.000000000f, 1.000000000f };
+}
