0c4c65fc01e9f69c855b4203864a7cc72ffb1859
Gaiyitp9 <932404058@qq.com>
2022-06-22 19:02:34 +0800

    更新Quaternion

--- RendererCore/include/Math/Quaternion.hpp
+++ RendererCore/include/Math/Quaternion.hpp
@@ -5,13 +5,22 @@
 *****************************************************************/
 #pragma once
 
+#include "Simd.hpp"
+
 namespace LCH::Math
 {
-	template <typename T>
+	template <typename T> requires std::is_same_v<T, double> || std::is_same_v<T, float>
 	class Quaternion
 	{
+		using simd = simd_trait<T, SimdInstruction<T, 4>::type>;
 	public:
 		Quaternion();
 		Quaternion(T x, T y, T z, T w);
+
+	private:
+		aligned_array<T, 4, simd::Alignment> elements;
 	};
+
+	using Quaternionf = Quaternion<float>;
+	using Quaterniond = Quaternion<double>;
 }
