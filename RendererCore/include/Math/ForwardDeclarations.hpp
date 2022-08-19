1f71665e75e678cf3be723d98d2b094089226098
Gaiyitp9 <932404058@qq.com>
2022-08-19 18:08:43 +0800

    更新数学库，增加evaluator

--- RendererCore/include/Math/ForwardDeclarations.hpp
+++ RendererCore/include/Math/ForwardDeclarations.hpp
@@ -7,18 +7,27 @@
 
 namespace LCH::Math
 {
+	// 类特性
 	template<typename T> struct traits;
 	template<typename T> struct traits<const T> : traits<T> {};
 
+	// 矩阵及矩阵表达式基类
 	template<typename Derived> class MatrixBase;
+	// 矩阵类
 	template<typename ScalarT, int Rows, int Cols, 
 		int Options_ = (Rows == 1 && Cols != 1 ? StorageOption::RowMajor :
 						Rows != 1 && Cols == 1 ? StorageOption::ColMajor :
 						DefaultMatrixStorageOrderOption)
 	> class Matrix;
-
+	// 矩阵底层存储类
 	template<typename T, int Size, int Rows, int Cols, int Options> class Storage;
 
+	// 二元运算
 	template<typename BinaryOp, typename Lhs, typename Rhs> class CwiseBinaryOp;
+	// 加法运算
 	template<typename LhsScalar, typename RhsScalar> struct scalar_sum_op;
+
+	// 表达式求值器
+	template<typename T> struct evaluator;
+	template<typename T> struct evaluator<const T> : evaluator<T> {};
 }
