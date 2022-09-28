178abcfd63e50d2117ac6d7ce194d7e88fc2206b
Gaiyitp9 <932404058@qq.com>
2022-09-28 11:11:43 +0800

    修改数学库的开源协议，改回MPL2.0

--- RendererCore/include/Math/ForwardDeclarations.hpp
+++ RendererCore/include/Math/ForwardDeclarations.hpp
@@ -1,13 +1,17 @@
-/********************************************************************
-* TianGong RenderLab												*
-* Copyright (c) Gaiyitp9. All rights reserved.						*
-* This code is licensed under the MIT License (MIT).				*
-*																	*
-* Noted:															*
-* This file is part of Eigen, a lightweight C++ template library	*
-* for linear algebra which is subject to the terms of the			*
-* Mozilla Public License v.2.0. And I made some simplifications.	*
-*********************************************************************/
+/************************************************************************
+* This file is part of Eigen, a lightweight C++ template library		*
+* for linear algebra.													*
+*																		*
+* Copyright (C) 2011 Benoit Jacob <jacob.benoit.1@gmail.com>			*
+* Copyright (C) 2011-2014 Gael Guennebaud <gael.guennebaud@inria.fr>	*
+* Copyright (C) 2011-2012 Jitse Niesen <jitse@maths.leeds.ac.uk>		*
+*																		*
+* This Source Code Form is subject to the terms of the Mozilla			*
+* Public License v. 2.0. If a copy of the MPL was not distributed		*
+* with this file, You can obtain one at http://mozilla.org/MPL/2.0/.	*
+*																		*
+* Noted: I made some modifications in this file.						*
+*************************************************************************/
 #pragma once
 
 namespace LCH::Math
