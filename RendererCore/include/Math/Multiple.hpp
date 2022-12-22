fabfe7be0f3a857f911f935dbd8c12293934a6d5
Gaiyitp9 <932404058@qq.com>
2022-12-22 20:05:20 +0800

    开始更新数学库的矩阵乘法运算

--- RendererCore/include/Math/Multiple.hpp
+++ RendererCore/include/Math/Multiple.hpp
@@ -7,5 +7,24 @@
 
 namespace TG::Math
 {
+	template<typename Matrix, int Row, int Col, int Rows, int Cols>
+	struct DefaultMultiple
+	{
 
+	};
+
+	template<typename Matrix, int Rows, int Cols>
+	struct DefaultMultiple<Matrix, Rows, Cols, Rows, Cols>
+	{
+
+	};
+
+	template<typename Matrix>
+	struct Multiple
+	{
+		static void Run()
+		{
+
+		}
+	};
 }
